309d8b20a60049caca27c8cf1967ce92f4a51a36
AVS-2307 <andrey230781.an@gmail.com>
2023-02-21 13:17:51 +0300

    add function ReplyToUser for DRY

--- task.js
+++ task.js
@@ -5,24 +5,26 @@ function game() {
 
     for (let i=1; i < 10; i++) {
         let getHole = i => document.getElementById(`hole${i}`)
+
+        function replyToUser(text) {
+            alert(text);
+            dead.textContent = 0;
+            lost.textContent = 0;
+        }
                       
         document.getElementById(`hole${i}`).onclick = function() {
  
             if (getHole(i).className.includes('hole_has-mole')) {
                 dead.textContent = Number(dead.textContent) + 1;
                 if (Number(dead.textContent) === 10) {
-                    alert('Вы победили!')
-                    dead.textContent = 0;
-                    lost.textContent = 0;          
+                    replyToUser('Вы победили!')
                 }   
             }
 
             else {
                 lost.textContent = Number(lost.textContent) + 1;
                 if (Number(lost.textContent) === 5) {
-                    alert('Вы проиграли :(')
-                    dead.textContent = 0;
-                    lost.textContent = 0;    
+                    replyToUser('Вы проиграли ;(')   
                 }
             }                       
         };
